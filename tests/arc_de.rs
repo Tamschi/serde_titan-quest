36652dab1468610a6088b799c47a4cc721f82473
Tamme Schichler <tamme@schichler.dev>
2020-08-13 14:41:07 +0200

    Improved test performace

--- tests/arc_de.rs
+++ tests/arc_de.rs
@@ -1,13 +1,19 @@
 use {
+    serde::de,
     serde_backtrace::serde_backtrace,
-    serde_object::Object,
     serde_titan_quest::arc,
-    std::{fs::File, path::Path},
+    std::{fs::File, io::BufReader, path::Path},
     test_case::test_case,
 };
 
 fn read_arc(path: impl AsRef<Path>) {
-    let _: Object = serde_backtrace(arc::from_read_seek(&mut File::open(path).unwrap()).unwrap());
+    let _: () = serde_backtrace(
+        arc::from_read_seek_seed(
+            &mut BufReader::new(File::open(&path).unwrap()),
+            serde_backtrace::Seed(Seed(path)),
+        )
+        .unwrap(),
+    );
 }
 
 #[test_case("Creatures.arc")]
@@ -60,4 +66,38 @@ fn dbg() {
 #[ignore = "requires Titan Quest Anniversary Edition to be installed"]
 fn anniversary_edition(path: impl AsRef<Path>) {
     read_arc(Path::new(r"C:\Program Files (x86)\Steam\steamapps\common\Titan Quest Anniversary Edition\Resources\").join(path));
+}
+
+struct Seed<P>(P);
+impl<'de, P> de::DeserializeSeed<'de> for Seed<P> {
+    type Value = ();
+    fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_map(Visitor(self.0))
+    }
+}
+
+struct Visitor<P>(P);
+impl<'de, P> de::Visitor<'de> for Visitor<P> {
+    type Value = ();
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(formatter, "ARC map")
+    }
+
+    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+    where
+        A: de::MapAccess<'de>,
+    {
+        while let Some(k) = map.next_key()? {
+            let k: String = k;
+            let data: Vec<u8> = map.next_value()?;
+            if data.is_empty() {
+                println!("{:?}", (data.len(), k));
+            }
+        }
+
+        Ok(())
+    }
 }
